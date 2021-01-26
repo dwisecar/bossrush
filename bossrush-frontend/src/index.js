a547851d713c12309227aa8bc3f3adbc3c4a9803
dwisecar <dwisecar@gmail.com>
2021-01-25 22:13:17 -0800

    fixed fetches for patching hero and enemy

--- bossrush-frontend/src/index.js
+++ bossrush-frontend/src/index.js
@@ -1,13 +1,25 @@
+//sequence
+//1. enemy dies
+//2. enemy name change patch
+//3. update hero score (enemy health * 100) + hero.score
+//4. fetch battle won
+//5. load new enemy
+
 //DATA
 
-let heroTurn = true 
+function handleEnemyDeath(){
+    Promise.all([updateEnemyName(), updateHeroScore(), fetchBattleWon(), fetchEnemy()])
+}
 
+let heroTurn = true 
+// gets users with highest scores
 function fetchHighScores(){
     fetch('http://localhost:3000/high_scores')
     .then(res => res.json())
     .then(heros => heros.forEach(hero => addHighScore(hero)))
 }
 
+//on form submission, posts new hero to database and calls fetch enemy
 function postHero(hero){
     fetch('http://localhost:3000/heros', {
         method: 'POST',
@@ -24,7 +36,32 @@ function postHero(hero){
     })
 }
 
-function postBattle(heroId, enemyId){
+//posts new enemy generated with faker data in enemies controller
+function fetchEnemy() {    
+    const enemy = document.querySelector('.enemy-card')
+    if(enemy){
+        enemy.remove()
+    }   
+
+    fetch('http://localhost:3000/enemies', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            Accept: 'application/json'
+        }
+    }).then(res => res.json())
+    .then(enemy => {
+        renderEnemy(enemy)
+        postBattle()
+    })
+}
+
+//runs after enemy card is rendered, posts record of battle
+function postBattle(){
+    const hero = document.querySelector('.hero-card') 
+    const heroId = parseInt(hero.dataset.id)
+    const enemy = document.querySelector('.enemy-card') 
+    const enemyId = parseInt(enemy.dataset.id)
     fetch('http://localhost:3000/battles', {
         method: 'POST',
         headers: {
@@ -35,22 +72,14 @@ function postBattle(heroId, enemyId){
             hero: heroId,
             enemy: enemyId
         })
-    })
+    }).then(console.log("Battle created"))
 }
 
-function fetchEnemy() {
-    fetch('http://localhost:3000/enemies', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify()
-    }).then(res => res.json())
-    .then(enemy => renderEnemy(enemy))
-}
 
-function updateHeroScore(heroId, newScore){
+//patch to the hero score attribute
+function updateHeroScore(){
+    const heroCard = document.querySelector('.hero-card')
+    const heroId = parseInt(heroCard.dataset.id)
     fetch(`http://localhost:3000/heros/${heroId}`, {
         method: 'PATCH',
         headers: {
@@ -59,14 +88,51 @@ function updateHeroScore(heroId, newScore){
         },
         body: JSON.stringify({
             id: heroId,
-            score: newScore
+            score: 100
         })
-    }).then()
+    }).then(res => res.json())
+    .then(hero => {
+        renderCurrentScore(hero.score)
+        fetchBattleWon()})
 }
 
+//changes enemy name to defeated
+function updateEnemyName(){
+    const enemyCard = document.querySelector('.enemy-card')
+    const enemyId = parseInt(enemyCard.dataset.id)
+    const enemyName = document.getElementById('enemy-name')
+    enemyName.innerText += ' Defeated'
+    fetch(`http://localhost:3000/enemies/${enemyId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            id: enemyId,
+            name: enemyName.innerText
+        })
+    }).then(() => {
+        console.log(`updated enemy name`)
+        raiseEnemyDefeatedToast()
+    })
+
+}
+
+//takes most recent battle won and puts it in the sidebar
+function fetchBattleWon(){
+    //get hero id
+    const heroCard = document.querySelector('.hero-card')
+    const heroId = parseInt(heroCard.dataset.id)
+    //fetch last battle hero won
+    fetch(`http://localhost:3000/heros/${heroId}`)
+    .then(res => res.json())
+    .then(battle => {
+        console.log(battle.enemy.name)
+        updateDefeatedEnemiesList(battle)})
+}
 
 //DOM
-
 function addListenerHeroForm(){
     const form = document.querySelector('form')
     form.addEventListener('submit', createHero)  
@@ -76,7 +142,6 @@ function renderBattleHeroCard(hero){
     const main = document.querySelector('main')
     const div = document.createElement('div')
     div.className = 'hero-card'
-
     div.dataset.id = hero.id
     
     const img = document.createElement('img')
@@ -114,22 +179,27 @@ function renderEnemy(enemy){
     img.src = enemy.image
     const name = document.createElement('p')
     name.innerText = enemy.name
+    name.id = 'enemy-name'
     const health = document.createElement('p')
     health.innerText = `Health: ${enemy.health}`
     health.id = 'enemy-health'
     div.append(name, img, health)
-    main.append(div)
+    main.append(div) 
 
-    const hero = document.querySelector('.hero-card')    
-    postBattle(parseInt(hero.dataset.id), enemy.id)
+    changeHeaderString('BOSS RUSH')
+    enableAttackButtons() 
 }
 
-function killEnemy(){
-    const enemy = document.querySelector('.enemy-card')
-    enemy.remove()
-    //add function to update enemy name to -defeated
-    //add battle.enemy to sidebar
-    fetchEnemy()
+function updateDefeatedEnemiesList(battle){
+    const ul = document.querySelector('.enemies-defeated')
+    const li = document.createElement('li')
+    
+    li.innerText = battle.enemy.name
+    const img = document.createElement('img')
+    img.src = battle.enemy.image
+    li.append(img)
+    ul.append(li)
+    setTimeout(fetchEnemy, 1000)
 }
 
 function addHighScore(hero){
@@ -139,12 +209,35 @@ function addHighScore(hero){
     ul.append(li)
 }
 
-function updateScore(points){
+function renderCurrentScore(points){
     const score = document.querySelector('.current-score')
     let newScore = parseInt(score.innerText) + points
     score.innerText = newScore
-    const hero = document.querySelector('.hero-card')
-    updateHeroScore(parseInt(hero.dataset.id), newScore)
+}
+
+function raiseEnemyDefeatedToast(){
+    changeHeaderString('Enemy Defeated')
+    setTimeout(updateHeroScore, 2000)
+    //graphical indication that enemy has been defeated
+}
+
+function disableAttackButtons(){
+    const btnA = document.getElementById('melee-attack-btn')
+    const btnB = document.getElementById('ranged-attack-btn')
+    btnA.disabled = true
+    btnB.disabled = true
+}
+
+function enableAttackButtons(){
+    const btnA = document.getElementById('melee-attack-btn')
+    const btnB = document.getElementById('ranged-attack-btn')
+    btnA.disabled = false
+    btnB.disabled = false
+}
+
+function changeHeaderString(string){
+    const h2 = document.getElementById('header-text')
+    h2.innerText = string
 }
 
 
@@ -173,8 +266,7 @@ function disableCreateHeroForm(){
     form.remove()
 }
 
-function checkOkToAttack(e){
-    
+function checkOkToAttack(e){    
     if (heroTurn){
         let damage = 0
         if(e.target.id == 'melee-attack-btn') {
@@ -186,12 +278,14 @@ function checkOkToAttack(e){
         let enemyHealth = parseInt(enemy.innerText.split(' ')[1])
         
         if(enemyHealth - damage < 1) {
-            killEnemy()
+            enemy.innerText = 'Health: 0'
+            disableAttackButtons()
+            updateEnemyName()
         }
         else{ 
             enemy.innerText = `Health: ${enemyHealth - damage}`
         }
-        updateScore((damage * 100))
+        
         //function to disable hero attack buttons
         //function to start enemy attacking
 
